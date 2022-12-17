46b68a19d9eb0bf290b2d033da469cb3f728b2ad
ChandaHGunaga <116174232+ChandaHGunaga@users.noreply.github.com>
2022-12-17 17:50:36 +0530

    newww

---
+++ src/redux/store.jsx
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit'
+import EmailSlice from './reducers/EmailSlice'
+import LoginSlice from './reducers/LoginSlice'
+import profileSlice from './reducers/profileSlice'
+import regDetailsSlice from './reducers/regDetailSlice'
+import studentlistSlice from './reducers/studentlistSlice'
+import superAdminInfo from './reducers/superAdminInfo'
+
+import showProfileSlice from './showProfile'
+
+export const store = configureStore({
+  reducer: {
+    showProfile: showProfileSlice.reducer,
+    Login: LoginSlice.reducer,
+    emailSend: EmailSlice.reducer,
+    regDetails: regDetailsSlice.reducer,
+    superAdmin: superAdminInfo.reducer,
+    profile: profileSlice.reducer,
+    studentList: studentlistSlice.reducer,
+  },
+})
